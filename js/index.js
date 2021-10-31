5fa281a0d6363fcb8da138e5f8c6c81bd6d040b4
Akif ÜNAL <akifunal05@gmail.com>
2021-10-31 23:55:54 +0300

    refactored jquery code to vanilla js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-window.onscroll = function () { navbarScroll() };
+window.addEventListener('load', navbarScroll)
 
 // OnScroll Navbar event
 function navbarScroll() {
-   if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-       // On Scroll
-       $("nav").addClass("scrolled-nav-bar");
-   } else {
-       // On Start
-       $("nav").removeClass("scrolled-nav-bar");
-   }
-};
+	if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+		// On Scroll
+		document.querySelector('nav').classList.add('navbar-scroll')
+	} else {
+		// On Start
+		document.querySelector('nav').classList.remove('scrolled-nav-bar')
+	}
+}
